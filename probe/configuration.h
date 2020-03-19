dae4dd30b231ccc440b2822a2bb6a91b4e394007
thvanarkel <me@thomasvanarkel.nl>
2020-03-19 16:20:52 +0100

    Move configuration to configuration.h

--- probe/configuration.h
+++
@@ -1,9 +0,0 @@
-//#define SECRET_SSID "vanArkeliPhone"
-//#define SECRET_PASS "ievk48?5shu"
-//#define SECRET_SSID "iot-net"
-//#define SECRET_PASS "ievk48?5shu"
-#define SECRET_SSID "Poppesteeg 1F Extended"
-#define SECRET_PASS "rYZ730SzY199"
-
-#define SECRET_USERNAME "data-probe"
-#define SECRET_PASSWORD "4ba28ccf92cd6901"
