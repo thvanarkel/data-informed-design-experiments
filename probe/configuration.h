3f57f7b45b916502512447888041185f1e59fe3d
thvanarkel <me@thomasvanarkel.nl>
2020-03-19 16:28:13 +0100

    Revert "Move configuration to configuration.h"
    This reverts commit dae4dd30b231ccc440b2822a2bb6a91b4e394007.
    
    Revert

---
+++ probe/configuration.h
@@ -0,0 +1,9 @@
+//#define SECRET_SSID "vanArkeliPhone"
+//#define SECRET_PASS "ievk48?5shu"
+//#define SECRET_SSID "iot-net"
+//#define SECRET_PASS "ievk48?5shu"
+#define SECRET_SSID "Poppesteeg 1F Extended"
+#define SECRET_PASS "rYZ730SzY199"
+
+#define SECRET_USERNAME "data-probe"
+#define SECRET_PASSWORD "4ba28ccf92cd6901"
