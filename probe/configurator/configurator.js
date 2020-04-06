39c575098d8ccb1d02d630927cbaa14b69cfa9e4
thvanarkel <me@thomasvanarkel.nl>
2020-04-06 15:47:17 +0200

    Implement inquirer

---
+++ probe/configurator/configurator.js
@@ -0,0 +1,26 @@
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const inquirer = require('./lib/inquirer');
+
+clear();
+
+console.log(
+  chalk.cyan(
+    figlet.textSync('probe', { font: 'isometric3', horizontalLayout: 'full' })
+  )
+);
+
+console.log(
+  chalk.cyan(
+    figlet.textSync(' -configurator- ', { font: 'cybermedium', horizontalLayout: 'full' })
+  )
+);
+
+
+const run = async () => {
+  const credentials = await inquirer.selectSensors(["motion", "temperature"]);
+  console.log(credentials);
+};
+
+run();
---
+++ probe/configurator/lib/inquirer.js
@@ -0,0 +1,42 @@
+const inquirer = require('inquirer');
+
+module.exports = {
+  askGithubCredentials: () => {
+    const questions = [{
+        name: 'username',
+        type: 'input',
+        message: 'Enter your GitHub username or e-mail address:',
+        validate: function(value) {
+          if (value.length) {
+            return true;
+          } else {
+            return 'Please enter your username or e-mail address.';
+          }
+        }
+      },
+      {
+        name: 'password',
+        type: 'password',
+        message: 'Enter your password:',
+        validate: function(value) {
+          if (value.length) {
+            return true;
+          } else {
+            return 'Please enter your password.';
+          }
+        }
+      }
+    ];
+    return inquirer.prompt(questions);
+  },
+  selectSensors: (sensors) => {
+    const questions = [{
+      type: 'checkbox',
+      name: 'ignore',
+      message: 'Select the files and/or folders you wish to ignore:',
+      choices: sensors ,
+      default: ['node_modules', 'bower_components']
+    }];
+    return inquirer.prompt(questions);
+  }
+};
